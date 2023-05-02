9bef8e81bf37891604928a5091e4c2c6b8feb225
svetlichnaya.org@gmail.com <svetlichnaya.org@gmail.com>
2023-05-02 13:22:08 +0200

    changing

--- SumForEachOrder.sql
+++ SumForEachOrder.sql
@@ -1,3 +1,4 @@
-SELECT O.OrderID, sum(P.Price) as sum FROM [OrderDetails] O -- O - OrderDetails
+SELECT O.OrderID, sum(O.Quantity) as Quantity_by_Order, sum(P.Price) as Summe, sum(O.Quantity * P.Price) as total_sum FROM [OrderDetails] O -- O - OrderDetails
 LEFT JOIN Products P ON O.ProductID = P.ProductID -- P - Products
-Group By O.OrderID
+Group By O.OrderID -- Group by OrderID
+Order By O.OrderID 
